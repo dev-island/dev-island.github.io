73cbc08182ea0af840350858564d76894319a397
AJ Webb <webbushka@gmail.com>
2021-10-28 20:16:04 -1000

    add GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,11 +5,20 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/assets/images`
-      }
+        path: `${__dirname}/src/assets/images`,
+      },
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    `gatsby-plugin-sass`
-  ]
-};
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-XQL6NF7Y9V", // Google Analytics / GA
+        ],
+      },
+    },
+  ],
+}
